0309df85d333f2716480502b6cd7ad78a25f39a9
ndbaolam <baolam782004@gmail.com>
2025-12-05 14:53:33 +0700

    OK

--- app/test.py
+++ app/test.py
@@ -1,21 +1,15 @@
 from pyspark.sql import SparkSession
 
-spark_version = '3.5.7'
+spark = SparkSession.builder.getOrCreate()
+sc = spark.sparkContext
 
-packages = [
-    'org.apache.hadoop.fs.s3a.S3AFileSystem',
-]
-
-spark = (
-    SparkSession.builder.appName("TestApp")
-    # .config("spark.jars.packages", ",".join(packages))               
-    .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000/")
-    .config("spark.hadoop.fs.s3a.access.key", "console")
-    .config("spark.hadoop.fs.s3a.secret.key", "console123")
-    .config("spark.hadoop.fs.s3a.path.style.access", "true")    
-    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-    .getOrCreate()
-)
+sc._jsc.hadoopConfiguration().set("fs.s3a.access.key", "console")
+sc._jsc.hadoopConfiguration().set("fs.s3a.secret.key", "console123")
+sc._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "http://minio:9000")
+sc._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "true")
+sc._jsc.hadoopConfiguration().set("fs.s3a.connection.ssl.enabled", "false")
+sc._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+sc._jsc.hadoopConfiguration().set("fs.s3a.connection.ssl.enabled", "false")
 
 df = spark.createDataFrame(
     [(1, "Alice"), (2, "Bob"), (3, "Cathy")],
